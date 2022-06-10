e0631886745000117a17698784e8389149246beb
Keitling <105679193+Keitling@users.noreply.github.com>
2022-06-10 17:17:39 -0400

    Add files via upload

---
+++ PREGUNTA1_2DOP_1RAF.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 17:08:12 2022
+
+@author: Flores Salinas
+"""
+
+from math import sqrt
+def F(n):
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
+
+def Fibonacci(startNumber, endNumber):
+    n = 0
+    cur = F(n)
+    while cur <= endNumber:
+        if startNumber <= cur:
+            print(cur)
+        n += 1
+        cur = F(n)
+Fibonacci(1,100)
