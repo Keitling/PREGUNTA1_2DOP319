3972379280d4c9c68e528435e113c2584fc1365f
Keitling <105679193+Keitling@users.noreply.github.com>
2022-06-15 23:24:30 -0400

    Add files via upload

---
+++ pregunta1_F#.fs
@@ -0,0 +1,8 @@
+﻿// Más información acerca de F# en http://fsharp.net
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+module fibo =
+let nums=[0..10]
+let rec fib n = if n < 1 then 0 else if n < 2 then 1 else fib (n-2) + fib(n-1)
+let finres = List.map fib nums
+printfn "%A" finres
+let _ = System.Console.ReadLine()
